bf6120c1ad78a8b0c03051b9f5db06243718173e
naioooo <34563332+naioooo@users.noreply.github.com>
2025-01-08 17:38:47 +0900

    프로젝트 추가

---
+++ Source/PUBG_KI/PUBG_KI.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PUBG_KI.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PUBG_KI, "PUBG_KI" );
+ 
---
+++ Source/PUBG_KI/PUBG_KIGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PUBG_KIGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APUBG_KIGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APUBG_KIGameMode();
+};
+
+
+
